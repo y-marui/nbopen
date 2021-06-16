c35ba0a5d9ee21926d44658599803d7a98b7f642
Yukihiro Marui <12211343+y-marui@users.noreply.github.com>
2021-06-16 22:12:43 +0900

    Fix bugs to work on Windows10

--- nbopen/nbopen.py
+++ nbopen/nbopen.py
@@ -10,6 +10,12 @@ import time
 import timeout_decorator
 from traitlets.config.configurable import Configurable
 from traitlets import Int
+import platform
+import tempfile
+import subprocess
+import os
+from pathlib import Path
+import time
 
 from .traitlets import ModeString
 
@@ -31,7 +37,7 @@ def find_best_server(filename: str) -> int:
         return None
 
 
-@timeout_decorator.timeout(opener.timeout)
+# @timeout_decorator.timeout(opener.timeout, use_signals=False)
 def wait_best_server(filename: str, server_inf):
     while server_inf is None:
         time.sleep(0.1)
@@ -53,11 +59,20 @@ def nbopen(filename: str):
         # to get server with find_best_server,
         # launch app by jupyter-notebook
         # even if you use JupyterLab
-        subprocess.Popen(["jupyter-notebook", "--no-browser", nbdir],
+        pf = platform.system()
+        if pf == 'Windows':
+            with tempfile.NamedTemporaryFile("w", suffix=".vbs", delete=False) as fp:
+                fp.write('Set ws = CreateObject("Wscript.Shell")\n'
+                         f'ws.run "cmd /c jupyter-notebook.bat {nbdir} --no-browser", vbhide')
+                subprocess.Popen(["cscript.exe", fp.name], shell=True)
+            server_inf = wait_best_server(filename, server_inf)
+            os.unlink(fp.name)
+        else:
+            subprocess.Popen(["jupyter-notebook", "--no-browser", nbdir],
                          stdout=subprocess.DEVNULL,
-                         stderr=subprocess.DEVNULL)
-
-    server_inf = wait_best_server(filename, server_inf)
+                         stderr=subprocess.DEVNULL, shell=True)
+            server_inf = wait_best_server(filename, server_inf)
+    
 
     print("Using existing server at", server_inf['notebook_dir'])
     path = os.path.relpath(filename, start=server_inf['notebook_dir'])
